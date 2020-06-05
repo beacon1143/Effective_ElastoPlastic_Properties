27c69606ee4bb92a908770cac088cd75c190b7a0
Maxim Yakovlev <beacon1143@gmail.com>
2020-06-05 21:57:23 +0300

    fix Hooke's law

--- SigmaCalc.cpp
+++ SigmaCalc.cpp
@@ -123,7 +123,7 @@ void SigmaCalc::ComputeSigma(const double loadValue, const std::array<int, 3>& l
       for (int j = 0; j < inp->nY; j++) {
         P[i][j] = Pinit[i][j] - inp->K[i][j] * divU[i][j];
         tauXX[i][j] = 2.0 * inp->G[i][j] * ( (Ux[i+1][j] - Ux[i][j]) / inp->dX - divU[i][j]/3.0);
-        tauYY[i][j] = 2.0 * inp->G[i][j] * ( (Uy[i][j+1] - Ux[i][j]) / inp->dY - divU[i][j]/3.0);
+        tauYY[i][j] = 2.0 * inp->G[i][j] * ( (Uy[i][j+1] - Uy[i][j]) / inp->dY - divU[i][j]/3.0);
         if (i < inp->nX - 1 && j < inp->nY - 1) {
           tauXY[i][j] = inp->Gmax * ( (Ux[i+1][j+1] - Ux[i+1][j]) / inp->dY + (Uy[i+1][j+1] - Uy[i][j+1]) / inp->dX );
         }
